3c9f7ece6c6d001230ec69ea5a394b0f327c51cd
rsbaher <reginabaher@gmail.com>
2018-03-07 22:23:44 -0700

    can message users and nice UI except png not loading

--- index.js
+++ index.js
@@ -1,9 +1,42 @@
 var app = require('express')();
+var express = require('express');
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.use(express.static(__dirname + '/css'));
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+  });
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+// send event to everyone: io.emit('some event', { for: 'everyone' });
+// send to everyone except certain sockets:
+//io.on('connection', function(socket){
+  //socket.broadcast.emit('hi');
+//});
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
